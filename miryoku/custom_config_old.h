64644d743174315e8f1b45296037cd8f32eb5096
pestoAioli <rickster1724@gmail.com>
2025-08-18 23:31:15 -0700

    hi

--- miryoku/custom_config_old.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/miryoku
-
-// Enable secure Bluetooth for Pro
-#define CONFIG_BT_SMP 1
-#define CONFIG_BT_SIGNING 1
-#define CONFIG_BT_CTLR_LE_ENC 1
